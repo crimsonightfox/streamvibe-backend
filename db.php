3116d10ed883c1a367869e7496969f0cfaf416cc
crimsonightfox <124242476+crimsonightfox@users.noreply.github.com>
2026-04-13 00:19:58 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,8 @@
 <?php
-$conn = new mysqli(
-    "sql200.infinityfree.com",
-    "if0_40980290",
-    "md7vQo22wr",
-    "if0_40980290_StreamVibe_db"
-);
-
-if ($conn->connect_error) {
-    die(json_encode(['success' => false, 'error' => 'DB failed: ' . $conn->connect_error]));
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, __DIR__ . '/ca.pem', NULL, NULL);
+mysqli_real_connect($conn, "mysql-13f8e68-streamvibe.b.aivencloud.com", "avnadmin", "AVNS_-TbAYyJb4l9uqLCBW6w", "defaultdb", 26120, NULL, MYSQLI_CLIENT_SSL);
+if (mysqli_connect_error()) {
+    die(json_encode(['success' => false, 'error' => 'DB failed: ' . mysqli_connect_error()]));
 }
 ?>
