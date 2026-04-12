f1e2e865d35cddc489cff977d628e6f50b9345b2
crimsonightfox <124242476+crimsonightfox@users.noreply.github.com>
2026-04-13 04:16:02 +0800

    Update start_stream.php

--- start_stream.php
+++ start_stream.php
@@ -1,4 +1,9 @@
 <?php
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: https://streamvibe.free.nf");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') { http_response_code(200); exit(); }
 include 'db.php';
 $title = $_POST['title'];
 $category = $_POST['category'];
